a19da59d15162d099b53eee3f6296fa4e1bd32a3
lukostry <lukasz.ostrowski85@gmail.com>
2016-07-29 16:53:22 +0200

    AppComponent fixed - OnInit import statement added at the top

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
